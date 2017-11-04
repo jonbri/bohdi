84fa0aba37a9ad9f445c87932b746d3ddb939477
Jonathan Brink <jonathandavidbrink@gmail.com>
2017-11-04 10:44:19 -0400

    subtraction module

--- tests.js
+++ tests.js
@@ -1,7 +1,13 @@
 const test = require('tape');
 const add = require('./lib/add');
+const sub = require('./lib/sub');
 
-test('basic test', function(t) {
+test('test add function', function(t) {
     t.equal(add(1,1), 2, 'result is correct');
+    t.end();
+});
+
+test('test sub function', function(t) {
+    t.equal(sub(1,1), 0, 'result is correct');
     t.end();
 });
