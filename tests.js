6f3f18458e0673c0d0c0be3381ebdd348af1aca7
Jonathan Brink <jonathandavidbrink@gmail.com>
2018-07-26 13:22:46 -0400

    nesting

--- tests.js
+++ tests.js
@@ -43,3 +43,133 @@ test('test map function - simple', function(t) {
     t.deepEqual(map(o0), oExpected, 'result is correct');
     t.end();
 });
+
+test('test map function - nesting', function(t) {
+    var o0 = {
+        items: [
+            {
+                name: 'jon',
+                color: 'blue',
+                items: [
+                    {
+                        name: 'tracy',
+                        color: 'purple'
+                    }
+                ]
+            },
+            {
+                name: 'bill',
+                color: 'red'
+            }
+        ]
+    };
+
+    var oExpected = {
+        ci: [
+            {
+                mingzi: 'jon',
+                yanse: 'blue',
+                ci: [
+                    {
+                        mingzi: 'tracy',
+                        yanse: 'purple'
+                    }
+                ]
+            },
+            {
+                mingzi: 'bill',
+                yanse: 'red'
+            }
+        ]
+    };
+
+    t.deepEqual(map(o0), oExpected, 'result is correct');
+    t.end();
+});
+
+test('test map function - deep nesting', function(t) {
+    var o0 = {
+        items: [
+            {
+                name: 'jojo',
+                color: 'olive'
+            },
+            {
+                name: 'jon',
+                color: 'blue',
+                items: [
+                    {
+                        name: 'tracy',
+                        color: 'purple',
+                        items: [
+                            {
+                                name: 'austin',
+                                color: 'black',
+                                items: [
+                                    {
+                                        name: 'jason',
+                                        color: 'gold'
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'bill',
+                color: 'red',
+                items: [
+                    {
+                        name: 'lucy',
+                        color: 'sliver'
+                    }
+                ]
+            }
+        ]
+    };
+
+    var oExpected = {
+        ci: [
+            {
+                mingzi: 'jojo',
+                yanse: 'olive'
+            },
+            {
+                mingzi: 'jon',
+                yanse: 'blue',
+                ci: [
+                    {
+                        mingzi: 'tracy',
+                        yanse: 'purple',
+                        ci: [
+                            {
+                                mingzi: 'austin',
+                                yanse: 'black',
+                                ci: [
+                                    {
+                                        mingzi: 'jason',
+                                        yanse: 'gold'
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                mingzi: 'bill',
+                yanse: 'red',
+                ci: [
+                    {
+                        mingzi: 'lucy',
+                        yanse: 'sliver'
+                    }
+                ]
+            }
+        ]
+    };
+
+    t.deepEqual(map(o0), oExpected, 'result is correct');
+    t.end();
+});
